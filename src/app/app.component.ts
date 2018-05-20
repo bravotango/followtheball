1c872a68a943dec60d3b6ab8143c320d5261e3ed
Bravo "Sasquatch" Tango <seattleriot@hotmail.com>
2018-05-19 17:06:31 -0700

    Initial check-in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+
+
+export class AppComponent implements OnInit {
+
+  constructor() {   }
+  urlVariable = '';
+  selectedLocation = '';
+  locations = ['Bellevue','Covington','Federal Way','Mill Creek','Northgate'];
+  
+  ngOnInit() {
+    console.log("ngOnInit - enter")
+  }
+
+  onClick(location): void {
+    console.log("onSelect - enter")
+    this.selectedLocation = location;
+    this.urlVariable = '../assets/' + location.replace(/\s+/g, '').toLowerCase() + '.jpg';
+    console.log('url: ' + this.urlVariable );
+  }
+
+  foo(location): void {
+    console.log("foo - enter")
+  }
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatCheckboxModule,MatButtonToggleModule,MatButtonModule,MatCardModule,MatToolbarModule,MatIconModule,MatMenuModule,MatSelectModule,MatTabsModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,MatButtonModule,MatCheckboxModule,MatCardModule,MatButtonToggleModule,MatToolbarModule,MatIconModule,MatMenuModule,BrowserAnimationsModule,MatSelectModule,MatTabsModule
   ],
+  exports:[MatButtonModule,MatCheckboxModule,MatCardModule,MatButtonToggleModule,MatToolbarModule,MatIconModule,MatMenuModule,BrowserAnimationsModule,MatSelectModule,MatTabsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
